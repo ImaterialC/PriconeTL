6d47193adf802986e23d08c61441fbd3b65d8fab
andyching168 <andyching168@gmail.com>
2022-05-18 01:41:35 +0800

    更新工具

--- tools/crop_and_assemble/opencv_makeimage.py
+++ tools/crop_and_assemble/opencv_makeimage.py
@@ -3,15 +3,63 @@ import json
 import time
 import numpy as np
 from datetime import datetime
+import ntpath
 
-print("Assamable images")
-img_height=input("Output image height:")
-img_width =input("Output image weight:")
-img_name=input("Output image name:")
+#圖片等比縮放
+def img_resize(image,h,w):
+    height, width = image.shape[0], image.shape[1]
+    # 设置新的图片分辨率框架
+    width_new = w
+    height_new = h
+    # 判断图片的长宽比率
+    if width / height >= width_new / height_new:
+        img_new = cv2.resize(image, (width_new, int(height * width_new / width)))
+    else:
+        img_new = cv2.resize(image, (int(width * height_new / height), height_new))
+    return img_new
+#圖片增加透明邊框
+def img_border(img,h,w):
+    borderValue1=0
+    borderValue2=0
+    borderValue3=0
+    borderValue4=0
+    size = img.shape
+    img_w= size[1]
+    img_h= size[0]
+    if (h-img_h)%2 ==0:
+        borderValue1=int((h-img_h)/2)
+        borderValue2=int((h-img_h)/2)
+    else:
+        borderValue1=int((h-img_h)/2+0.5)
+        borderValue2=int((h-img_h)/2-0.5)
+    if (w-img_w)%2 ==0:
+        borderValue3=int((w-img_w)/2)
+        borderValue4=int((w-img_w)/2)
+    else:
+        borderValue3=int((w-img_w)/2+0.5)
+        borderValue4=int((w-img_w)/2-0.5)
+    img_with_border = cv2.copyMakeBorder(img, borderValue1, borderValue2,borderValue3,borderValue4, cv2.BORDER_CONSTANT, value=[0,0,0,0])#上,下,左,右
+    return img_with_border
+
+
+
+
+
+
+
+print("Assamable images_new")
+TemplateImageSource=input("Output image Template:")
+imgTemp = cv2.imread(TemplateImageSource, cv2.IMREAD_UNCHANGED)
+sizeTemp = imgTemp.shape
+img_height=sizeTemp[0]
+img_width =sizeTemp[1]
+
+print(ntpath.basename(TemplateImageSource))
+img_name=ntpath.basename(TemplateImageSource)
 n_channels = 4
 transparent_img = np.zeros((int(img_height), int(img_width), n_channels), dtype=np.uint8)
 # Save the image for visualization
-cv2.imwrite(img_name, transparent_img)
+cv2.imwrite("output/"+img_name, transparent_img)
 json_file=input("Json file:")
 folderchoice=input("Use JP[1] or zh_TW[2] to make:")
 if folderchoice =="1":
@@ -56,31 +104,23 @@ for i in data['mSprites']:
         print("w="+str(w))
         print("h="+str(h))
         if img_w<=w and img_h<=h:
-            borderValue1=0
-            borderValue2=0
-            borderValue3=0
-            borderValue4=0
-            if (h-img_h)%2 ==0:
-                borderValue1=int((h-img_h)/2)
-                borderValue2=int((h-img_h)/2)
-            else:
-                borderValue1=int((h-img_h)/2+0.5)
-                borderValue2=int((h-img_h)/2-0.5)
-            if (w-img_w)%2 ==0:
-                borderValue3=int((w-img_w)/2)
-                borderValue4=int((w-img_w)/2)
-            else:
-                borderValue3=int((w-img_w)/2+0.5)
-                borderValue4=int((w-img_w)/2-0.5)
-            img_with_border = cv2.copyMakeBorder(img, borderValue1, borderValue2,borderValue3,borderValue4, cv2.BORDER_CONSTANT, value=[0,0,0,0])#上,下,左,右
-            OutputImage[y:y+h, x:x+w]=img_with_border
+            #圖片皆小於等於JSON紀載
+            OutputImage[y:y+h, x:x+w]=img_border(img,h,w)
             cv2.imwrite(img_name, OutputImage)
             time.sleep(1)
         else:
             print("---")
-            imageProcessed = cv2.resize(img, (w, h), interpolation=cv2.INTER_AREA)
-            OutputImage[y:y+h, x:x+w]=imageProcessed
-            cv2.imwrite(img_name, OutputImage)
+            #圖片長度或寬度大於JSON紀載，先等比縮小
+            imageProcessed = img_resize(img,h,w)
+            if w==img_w and h==img_h:
+                OutputImage[y:y+h, x:x+w]=imageProcessed
+                cv2.imwrite(img_name, OutputImage)
+            else:
+                OutputImage[y:y+h, x:x+w]=img_border(imageProcessed,h,w)
+                cv2.imwrite(img_name, OutputImage)
+            
+            
+
             
             
             print("You will need to check if the image stretch too much.")
